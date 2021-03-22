2a94442f65cf707e100b164df928b5da15d5fbca
= <=>
2021-03-22 18:09:14 -0300

    Revert "Add route ADM"
    This reverts commit 9515c197e761e6af14ead1d924fc34a1bb382f6d.

--- web/src/components/ADMComponent/ADMComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import CreateProductComponent from '../CreateProductComponent/CreateProductComponent';
-
-const ADMComponent = () => (
-  <CreateProductComponent />
-);
-export default ADMComponent;
