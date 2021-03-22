9515c197e761e6af14ead1d924fc34a1bb382f6d
= <=>
2021-03-22 18:03:05 -0300

    Add route ADM

---
+++ web/src/components/ADMComponent/ADMComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import CreateProductComponent from '../CreateProductComponent/CreateProductComponent';
+
+const ADMComponent = () => (
+  <CreateProductComponent />
+);
+export default ADMComponent;
